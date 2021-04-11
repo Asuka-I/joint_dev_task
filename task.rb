80ad832a12e4bfddfa92ec727cf8d9acda31ada5
Asuka-I <1400cm@gmail.com>
2021-04-12 00:09:44 +0900

    add answer Q5 ~ Q9

--- task.rb
+++ task.rb
@@ -41,6 +41,8 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
+  puts array1.empty?
+  puts array2.empty?
 
 end
 
@@ -48,6 +50,8 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
+  numbers2 = numbers1.map { |n| n * 10 }
+  p numbers2
 
 end
 
@@ -55,6 +59,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+  array.map!{|i| i.to_i}
 
   # 以下は変更しないで下さい
   p array
@@ -64,7 +69,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages.map!(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -74,6 +80,9 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 
 end
 
